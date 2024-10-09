d47626a4e381f9b5a9ebc93ee0d9a33dc1a4d0c8
Filip <filipgaciarz@gmail.com>
2024-10-09 11:47:45 +0200

    task 3 SDK config

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import argparse
 import boto3
 from botocore.exceptions import ClientError
+from dotenv import load_dotenv
 import logging
 import os
 import re
+
+load_dotenv('touch.env')
 
 s3 = boto3.client(
     's3',
     aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
     aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
-    region_name='us-east-1'
+    region_name=os.getenv('AWS_DEFAULT_REGION')
 )
 
 bucket_name = 'developer-task'
